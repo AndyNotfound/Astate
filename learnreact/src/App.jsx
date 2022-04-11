f5ae64a343f5107b41467c4ea2d2c08097e1e995
AndyNotfound <Andy.Notfound@gmail.com>
2022-04-12 01:16:43 +0700

    React foundation

---
+++ learnreact/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Nav from './components/nav/nav'
+import Hero from './components/hero/hero'
+import Advantage from './components/advantage/advantage'
+import Contact from './components/contact/contact'
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Advantage />
+      <Contact />
+    </>
+  )
+}
+
+export default App
---
+++ learnreact/src/components/advantage/advantage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './advantage.css'
+
+function Advantage() {
+  return (
+    <div>Advantage</div>
+  )
+}
+
+export default Advantage
