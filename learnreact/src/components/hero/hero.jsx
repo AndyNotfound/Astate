5344261fef6e9b96d2ef449da2763df6aa8c1b2f
AndyNotfound <Andy.Notfound@gmail.com>
2022-04-15 19:19:32 +0700

    Responsive hero section

--- learnreact/src/components/hero/hero.jsx
+++ learnreact/src/components/hero/hero.jsx
@@ -7,17 +7,18 @@ export const Hero = () => {
   return(
     <>
       <header id='hero'>
-        <div className='main'>
-          <h5>Now in <span className='warn'> 50% off</span></h5>
-          <h1>Find your modern and <span className='focus'> Affordable </span>property.</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        <div className='container container__header'>
+            <div className='main'>
+              <h5>Now in <span className='warn'> 50% off</span></h5>
+              <h1>Find your modern and <span className='focus'> Affordable </span>property.</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+            </div>
+            <div className='image'>
+              <img src={agent} />
+            </div>
         </div>
-        <div className='image'>
-          <img src={agent} />
-        </div>
-        < Search />
+        {/* < Search /> */}
       </header>
-      
   </>
   )
 }
