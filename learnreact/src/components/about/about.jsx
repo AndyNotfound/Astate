e1bb7e3b2f1b64aacfeaee56edb74581d07b718d
AndyNotfound <Andy.Notfound@gmail.com>
2022-04-22 00:29:54 +0700

    Created advantages grid

--- learnreact/src/components/about/about.jsx
+++ learnreact/src/components/about/about.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './about.css'
+import contract from '../../assets/contract.png'
+import commission from '../../assets/commission.png'
+import taxes from '../../assets/taxes.png'
 
 export const About = () => {
   return (
     <section id='about'>
-      <p>Halo</p>
-      {/* 
-      There's some issue we found, that is how we can't use container somehow
-      Please fix the issue*/}
+      <div className='advantage-grid'>
+        <div className='advantage'>
+            <img src={contract} className='advantage-icon' />
+            <h5>property<pre />insurance</h5>
+        </div>
+        <div className='advantage'>
+            <img src={taxes} className='advantage-icon' />
+            <h5>tax<pre />advantage</h5>
+        </div>
+        <div className='advantage'>
+            <img src={commission} className='advantage-icon' />
+            <h5>lowest<pre />commission</h5>
+        </div>
+      </div>
     </section>
   )
 }
