6bbb1d9cdb7309d7ab05b269ab02ce35f83bb6df
AndyNotfound <Andy.Notfound@gmail.com>
2022-04-29 20:02:57 +0700

    Responsive Service Section

---
+++ learnreact/src/components/service/cardbreakpoint.jsx
@@ -0,0 +1,26 @@
+const breakpoint = [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ];
+
+export default breakpoint
--- learnreact/src/components/service/service.jsx
+++ learnreact/src/components/service/service.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import Post from "./Post";
+import breakpoint from "./cardbreakpoint";
 import './service.css'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 import { GoLocation } from 'react-icons/go'
@@ -37,32 +38,7 @@ class Service extends Component {
       nextArrow: <SampleNextArrow />,
       prevArrow: <SamplePrevArrow />,
       className: 'post-card',
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
+      responsive: breakpoint
     };
 
     return (
@@ -73,28 +49,26 @@ class Service extends Component {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
           </div>
           <Slider {...settings}>
-          {Post.map((postedItem, idx) => {
+          {Post.map((postedItem, index) => {
               return(  
-                <div className="post">
-                   <div key={idx} className='post-box'>
-                  <div class="post-image">
-                    <figure>
-                      <img src={postedItem.img}  />
-                    </figure>
-                  </div>
-                  <div className="post-detail">
-                    <h5>{postedItem.title}</h5>
-                    <div className='post-location'>
-                      <GoLocation className='location-icon'/>
-                      <p>{postedItem.location}</p>
-                    </div>
-                    <div className="post-price">
-                      <h5>{postedItem.price}</h5>
-                      <a className='button' href={postedItem.link}>Read More</a>
-                    </div>
-                  </div>
-                </div>
-                </div>
+                <ul className="post">
+                   <li key={index} className='post-box'>
+                        <div class="post-image">
+                          <figure><img src={postedItem.img}  /></figure>
+                        </div>
+                        <div className="post-detail">
+                          <h5>{postedItem.title}</h5>
+                          <div className='post-location'>
+                            <GoLocation className='location-icon'/>
+                            <p>{postedItem.location}</p>
+                          </div>
+                          <div className="post-price">
+                            <h5>{postedItem.price}</h5>
+                            <a className='button' href={postedItem.link}>Read More</a>
+                          </div>
+                        </div>
+                    </li>
+                </ul>
               )
           })}
           </Slider>
